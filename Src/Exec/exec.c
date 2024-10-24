1e7cf02bb8beb1fa8dfb8edc8a977ed06695031a
jmsk8 <miccio.jskode@hotmail.com>
2024-10-24 18:19:23 +0200

    Small modifications to the function behavior to allow executing built-ins. Do not look at my ft_exit function

--- Src/Exec/exec.c
+++ Src/Exec/exec.c
@@ -6,13 +6,20 @@
 /*   By: j_sk8 <j_sk8@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 22:49:41 by j_sk8             #+#    #+#             */
-/*   Updated: 2024/10/23 15:34:38 by j_sk8            ###   ########.fr       */
+/*   Updated: 2024/10/24 18:07:28 by j_sk8            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	exec(t_data *data, char **env)
+int	exec_builtin(t_data *data)
+{
+	if (ft_strnstr((data->token->str), "exit", 4))
+		return (ft_exit(data));
+	return (1);
+}
+
+void	exec_cmd(t_data *data, char **env)
 {
 	data->pid = fork();
 	if (data->pid < 0)
@@ -30,11 +37,18 @@ void	exec(t_data *data, char **env)
 			perror("execve");
 		}
 		else
-		{
 			printf("Command not found: %s\n", data->args[0]);
-		}
 		exit(EXIT_FAILURE);
 	}
 	else
 		wait(NULL);
+}
+
+int	exec(t_data *data, char **env)
+{
+	if (data->token->is_builtin)
+		return (exec_builtin(data));
+	else
+		exec_cmd(data, env);
+	return (1);
 }
