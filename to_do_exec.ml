92c39e097413e3c3731d7c68614d6f57592a246a
Nathan Sauret <nsauret@made-f0Cr3s6.clusters.42paris.fr>
2024-11-07 16:11:03 +0100

    to_do_exec.ml updated

--- to_do_exec.ml
+++ to_do_exec.ml
@@ -1,6 +1,8 @@
 [X] - Replace my struct by the data variables for less memory leaks potential.
 [X] - Handle infiles and outfiles in exec.
 [ ] - "< Makefile cat | cat | cat | cat | cat > outfile1 > outfile2 > outfile3 | ls > outfile2" should work properly.
-[ ] - Redirection << focntionnelle.
-[ ] - Redirection >> fonctionnelle.
-[ ] - Utiliser les builtin lorsque c'est possible.
+[ ] - << redirection .
+[ ] - >> redirection.
+[ ] - Use builtins when possible.
+[ ] - Add sleep function.
+[ ] - When there is a problem with a file, make cmd==NULL to skip the command.
