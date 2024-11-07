251741895ddd4c5e73f997ba84b7723d4c14b721
Nathan Sauret <nsauret@made-f0Cr3s6.clusters.42paris.fr>
2024-11-07 16:05:55 +0100

    to_do_exec.ml updated

--- to_do.ml
+++
@@ -1,2 +0,0 @@
-[X] - Replace my struct by the data variables for less memory leaks potential.
-[ ] - Handle infiles and outfiles in exec.
---
+++ to_do_exec.ml
@@ -0,0 +1,6 @@
+[X] - Replace my struct by the data variables for less memory leaks potential.
+[X] - Handle infiles and outfiles in exec.
+[ ] - "< Makefile cat | cat | cat | cat | cat > outfile1 > outfile2 > outfile3 | ls > outfile2" should work properly.
+[ ] - Redirection << focntionnelle.
+[ ] - Redirection >> fonctionnelle.
+[ ] - Utiliser les builtin lorsque c'est possible.
