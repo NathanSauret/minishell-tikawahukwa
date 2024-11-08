1ba40c1b4172f64c73a692bd41e4a57f4d35956a
Nathan Sauret <nsauret@made-f0Br6s15.clusters.42paris.fr>
2024-11-08 12:28:59 +0100

    save

--- to_do_exec.ml
+++ to_do_exec.ml
@@ -1,8 +1,9 @@
 [X] - Replace my struct by the data variables for less memory leaks potential.
 [X] - Handle infiles and outfiles in exec.
+[X] - When there is a problem with a file, make exec->in==-1 and exec->out==-1 to skip the command.
+[ ] - >> redirection.
 [ ] - "< Makefile cat | cat | cat | cat | cat > outfile1 > outfile2 > outfile3 | ls > outfile2" should work properly.
 [ ] - << redirection .
-[ ] - >> redirection.
 [ ] - Use builtins when possible.
 [ ] - Add sleep function.
-[ ] - When there is a problem with a file, make exec->in==-1 and exec->out==-1 to skip the command.
+[ ] - "< Makefile cat | grep A > outfile1 > outfile2 > outfile3 | ls -l" sould work properly.
