41b6e5644c714abf2ebd6197f89349e4b4b9c89e
Nathan Sauret <nathan@nc-ass-vip.sdv.fr>
2024-11-21 14:56:57 +0100

    commit

--- Src/Exec/free_exec.c
+++ Src/Exec/free_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: j_sk8 <j_sk8@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nathan <nathan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 14:56:46 by nsauret           #+#    #+#             */
-/*   Updated: 2024/11/21 14:08:23 by j_sk8            ###   ########.fr       */
+/*   Updated: 2024/11/21 14:58:10 by nathan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	free_parent(t_pipex *pipex, t_data *data)
 
 int	free_pipe(t_pipex *pipex)
 {
-	if (*pipex->here_doc >= 0)
+	if (pipex->here_doc != NULL)
 		unlink(".heredoc");
 	free(pipex->pipe);
 	return (exit_error_exec(pipex, 0, "Environment"));
