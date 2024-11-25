593fc73a51b9ed78e0e920c2fb93f971127aa515
Nathan Sauret <nsauret@paul-f4Ar7s8.clusters.42paris.fr>
2024-11-25 18:27:11 +0100

    Now here_doc is in a pipe instead of a file; fds still leaks in  childs, but it's getting less worst /shrug

--- Src/Exec/free_exec.c
+++ Src/Exec/free_exec.c
@@ -6,7 +6,7 @@
 /*   By: nsauret <nsauret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 15:14:06 by nsauret           #+#    #+#             */
-/*   Updated: 2024/11/25 18:14:54 by nsauret          ###   ########.fr       */
+/*   Updated: 2024/11/25 18:27:02 by nsauret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	free_parent(t_pipex *pipex, t_data *data)
 {
-	if (pipex->here_doc)
-		unlink(".heredoc");
 	if (data->num_of_pipe > 0)
 		free(pipex->pipe);
 }
@@ -39,8 +37,6 @@ void	free_child(t_data *data, t_pipex *pipex)
 
 int	free_pipe(t_pipex *pipex)
 {
-	if (pipex->here_doc != NULL)
-		unlink(".heredoc");
 	free(pipex->pipe);
 	return (exit_error_exec(pipex, 0, "Environment"));
 }
