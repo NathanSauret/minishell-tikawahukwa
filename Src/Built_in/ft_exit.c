e7f9929fc4d844ae80a70e5894a9e0b8642d09f2
Nathan Sauret <nsauret@bess-f3r2s7.clusters.42paris.fr>
2024-12-12 19:00:58 +0100

    exit fixed

--- Src/Built_in/ft_exit.c
+++ Src/Built_in/ft_exit.c
@@ -3,33 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmiccio <jmiccio <marvin@42.fr>            +#+  +:+       +#+        */
+/*   By: nsauret <nsauret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:39:22 by j_sk8             #+#    #+#             */
-/*   Updated: 2024/12/11 11:52:11 by jmiccio          ###   ########.fr       */
+/*   Updated: 2024/12/12 18:44:54 by nsauret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+static long long	ft_lluatoi(const char *nptr)
+{
+	unsigned long long	num;
+	int					sign;
+
+	sign = 1;
+	num = 0;
+	while (ft_is_space(*nptr))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		num = num * 10 + (*nptr - '0');
+		if (sign == 1 && num > 9223372036854775807)
+			return (0);
+		else if (sign == -1 && num > 9223372036854775808LLU)
+			return (0);
+		nptr++;
+	}
+	return (num * sign);
+}
+
 static int	ft_is_exit_arg_a_number(char *arg)
 {
-	int	i;
+	int			i;
+	long long	numercic_arg;
 
-	if (arg[0] == '-' && ft_strncmp(arg, "-9223372036854775808", 21) > 0)
-		return (255);
-	else if (ft_strncmp(arg, "9223372036854775807", 20) > 0)
-		return (255);
+	numercic_arg = ft_lluatoi(arg);
+	if ((numercic_arg == 0 && arg[0] != '0' && ft_strlen(arg) != 1)
+		|| (ft_strlen(arg) == 1 && ft_is_space(arg[0])))
+		return (2);
 	i = 0;
 	while (arg[i])
 	{
 		if (!ft_isdigit(arg[i])
 			&& arg[i] != '"'
 			&& arg[i] != '-'
-			&& arg[i] != '+')
-		{
+			&& arg[i] != '+'
+			&& !ft_is_space(arg[i]))
 			return (2);
-		}
+		else if (arg[i + 1] && ft_is_space(arg[i]) && !ft_is_space(arg[i + 1]))
+			return (2);
 		i++;
 	}
 	return (0);
@@ -49,8 +78,6 @@ void	ft_exit(t_data *data, char **cmd)
 		}
 		check_res = ft_is_exit_arg_a_number(cmd[1]);
 		if (check_res == 2)
-			terminate(data, " numeric argument required\n", 2);
-		if (check_res == 255)
 			terminate(data, " numeric argument required\n", 2);
 		else
 		{
